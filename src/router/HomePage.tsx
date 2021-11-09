074d53a44e2f7fc62eac7fcf772b4b53a1f6d147
mtnhtmn <mtnhtmn@gmail.com>
2021-11-09 22:17:50 +0200

    first commit

---
+++ src/router/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import City from "../components/City";
+
+const HomePage = () => {
+    return (
+        <div>
+            <City/>
+        </div>
+    );
+};
+
+export default HomePage;
